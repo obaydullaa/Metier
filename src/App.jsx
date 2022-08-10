4c9c91ed1241e3850b054a71839285cb36b083c1
obaydullaa <cr7obaydul@gmail.com>
2022-08-10 09:58:27 +0600

    Adding Metier react app.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2>Hello from React</h2>
+    </>
+  )
+}
+
+export default App
