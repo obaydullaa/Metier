2a5fd034e6eae50e2a183210860702b4406d033d
obaydullaa <cr7obaydul@gmail.com>
2022-09-05 16:31:54 +0600

    Bug Fixed.

--- src/components/home/HomeFeatures.jsx
+++ src/components/home/HomeFeatures.jsx
@@ -109,65 +109,6 @@ function HomeFeatures() {
                 </div>
             </div>
         </div>
-    
-
-        {/* <div className="features__area">
-            <div className="container-fluid px-0">
-                <div className="row gx-0">
-                    <div className="col-xl-12">
-                        <div className="swiper features-slider">
-                            <div className="swiper-wrapper">
-
-                                <div  onMouseOver={() => handleHover(1)}  
-                                className={` ${isHovering === 1 ? activeStyle : ''} features__area-item swiper-slide `}> 
-                                
-                                <img src="assets/img/features-1.jpg" alt="" />
-                                    <div className="features__area-item-content">
-                                        <div className="features__area-item-content-icon"> <a href="services-details.html"><i className="fal fa-plus"></i></a> </div>
-                                        <h3><a href="services-details.html">Product Design</a></h3>
-                                        <p>Design strategy</p>
-                                    </div>
-                                </div>
-
-                                <div  onMouseOver={() => handleHover(2)}  
-                                className={` ${isHovering === 2 ? activeStyle : ''} features__area-item swiper-slide `}> 
-
-                                <img src="assets/img/features-2.jpg" alt="" />
-                                    <div className="features__area-item-content">
-                                        <div className="features__area-item-content-icon"> <a href="services-details.html"><i className="fal fa-plus"></i></a> </div>
-                                        <h3><a href="services-details.html">Product Design</a></h3>
-                                        <p>Design strategy</p>
-                                    </div>
-                                </div>
-
-                                <div  onMouseOver={() => handleHover(3)}  
-                                className={` ${isHovering === 3 ? activeStyle : ''} features__area-item swiper-slide `}>
-                                
-                                <img src="assets/img/features-3.jpg" alt="" />
-                                    <div className="features__area-item-content">
-                                        <div className="features__area-item-content-icon"> <a href="services-details.html"><i className="fal fa-plus"></i></a> </div>
-                                        <h3><a href="services-details.html">Product Design</a></h3>
-                                        <p>Design strategy</p>
-                                    </div>
-                                </div>
-
-                                <div  onMouseOver={() => handleHover(4)}  
-                                className={` ${isHovering === 4 ? activeStyle : ''} features__area-item swiper-slide `}>  
-                                
-                                <img src="assets/img/features-4.jpg" alt="" />
-                                    <div className="features__area-item-content">
-                                        <div className="features__area-item-content-icon"> <a href="services-details.html"><i className="fal fa-plus"></i></a> </div>
-                                        <h3><a href="services-details.html">Product Design</a></h3>
-                                        <p>Design strategy</p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> */}
     </>
     
   )
