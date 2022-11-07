e714839c04ffbae5a114b3bcae967ded4ad0ae32
obaydullaa <cr7obaydul@gmail.com>
2022-11-07 09:23:27 +0600

    Added Home three sponsor slider section.

--- src/components/homethree/HomeThreeSponsors.jsx
+++ src/components/homethree/HomeThreeSponsors.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
 
 function HomeThreeSponsors() {
   return (
@@ -7,22 +8,35 @@ function HomeThreeSponsors() {
             <div className="row">
                 <div className="col-xl-6"></div>
                 <div className="col-xl-6">
-                    <div className="swiper sponsors-slider">
-                        <div className="swiper-wrapper">
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-1.png"alt="" /></a>
-                            </div>
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-2.png"alt="" /></a>
-                            </div>
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-3.png"alt="" /></a>
-                            </div>
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-4.png"alt="" /></a>
-                            </div>
-                        </div>
-                    </div>
+                <Swiper
+                        slidesPerView={3}
+                        spaceBetween={0}
+                        loop={true}
+                        spaceBetween={50} 
+                        className="mySwiper" >
+                        <Swiper className="swiper-wrapper sponsors-slider">
+                            <SwiperSlide>
+                                <div className="sponsors__area-brand swiper-slide">
+                                    <a href="#"><img src="assets/img/brand-1.png" alt="" /></a>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="sponsors__area-brand swiper-slide">
+                                    <a href="#"><img src="assets/img/brand-2.png" alt="" /></a>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="sponsors__area-brand swiper-slide">
+                                    <a href="#"><img src="assets/img/brand-3.png" alt="" /></a>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="sponsors__area-brand swiper-slide">
+                                    <a href="#"><img src="assets/img/brand-4.png" alt="" /></a>
+                                </div>
+                            </SwiperSlide>
+                        </Swiper>
+                    </Swiper>
                 </div>
             </div>
         </div>
--- src/components/hometwo/HomeTwoSponsors.jsx
+++ src/components/hometwo/HomeTwoSponsors.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
 
 function HomeTwoSponsors() {
   return (
@@ -13,8 +12,7 @@ function HomeTwoSponsors() {
                         spaceBetween={0}
                         loop={true}
                         spaceBetween={50} 
-                        className="mySwiper"
-                    >
+                        className="mySwiper" >
                         <Swiper className="swiper-wrapper sponsors-slider">
                             <SwiperSlide>
                                 <div className="sponsors__area-brand swiper-slide">
@@ -37,24 +35,7 @@ function HomeTwoSponsors() {
                                 </div>
                             </SwiperSlide>
                         </Swiper>
-
                     </Swiper>
-                    {/* <div className="swiper sponsors-slider">
-                        <div className="swiper-wrapper">
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-1.png" alt="" /></a>
-                            </div>
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-2.png" alt="" /></a>
-                            </div>
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-3.png" alt="" /></a>
-                            </div>
-                            <div className="sponsors__area-brand swiper-slide">
-                                <a href="#"><img src="assets/img/brand-4.png" alt="" /></a>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
